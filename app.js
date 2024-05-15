c6e74ece9fcacab556bcf8b34f14e7966c3dfd32
Diego Lugo <contacto.dalo@gmail.com>
2024-05-15 08:28:43 -0600

    feat: Load menu data on landing

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
-window.addEventListener("DOMContentLoaded", () => {
+import Store from "./services/Store.js";
+import API from "./services/API.js";
+import { loadData } from "./services/Menu.js";
 
+window.app = {}
+app.store = Store;
+
+window.addEventListener("DOMContentLoaded", async () => {
+    loadData();
 });
